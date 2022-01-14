0dc1e73b77f62f8144b5b76afe96827d2bbeea3a
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-01-14 16:01:16 -0500

    feat: Introduce constants

---
+++ src/main/java/me/bdx/essentialsbungee/Utils/EssentialsBungeeConstants.java
@@ -0,0 +1,12 @@
+package me.bdx.essentialsbungee.Utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class EssentialsBungeeConstants {
+
+    public static String WHITELIST_COMMNAND_PERMISSION = "essentialsBungee.whitelist";
+
+    public static String WHITELIST_SUBCOMMAND_LIST_PERMISSION = "essentialsBungee.whitelist.list";
+
+    public static String MISSING_PERMISSION_RESPONSE = ChatColor.RED + "You do not have the required permissions to use this command!";
+}
