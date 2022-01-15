7445c81298719ba091a0617db4956466655b9667
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-01-14 21:53:30 -0500

    fix: Remove unused permission

--- src/main/java/me/bdx/essentialsbungee/Utils/EssentialsBungeeConstants.java
+++ src/main/java/me/bdx/essentialsbungee/Utils/EssentialsBungeeConstants.java
@@ -4,9 +4,7 @@ import net.md_5.bungee.api.ChatColor;
 
 public class EssentialsBungeeConstants {
 
-    public static String WHITELIST_COMMNAND_PERMISSION = "essentialsBungee.whitelist";
+    public static final String WHITELIST_COMMNAND_PERMISSION = "essentialsBungee.whitelist";
 
-    public static String WHITELIST_SUBCOMMAND_LIST_PERMISSION = "essentialsBungee.whitelist.list";
-
-    public static String MISSING_PERMISSION_RESPONSE = ChatColor.RED + "You do not have the required permissions to use this command!";
+    public static final String MISSING_PERMISSION_RESPONSE = ChatColor.RED + "You do not have the required permissions to use this command!";
 }
