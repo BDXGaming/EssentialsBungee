7702379f1d9450975e4424ed3b6bca6c2b9ca6a8
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-01-18 14:14:58 -0500

    feat: Add ChatUtils

---
+++ src/main/java/me/bdx/essentialsbungee/Utils/ChatUtils.java
@@ -0,0 +1,23 @@
+package me.bdx.essentialsbungee.Utils;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+public class ChatUtils {
+
+    /**
+     * When invoked this method sends a message to all online players with the given permission
+     * @param message String
+     * @param permission String
+     */
+    public static void broadcast(String message, String permission){
+
+        for(ProxiedPlayer p: ProxyServer.getInstance().getPlayers()){
+            if(p.hasPermission(permission)){
+                p.sendMessage(new TextComponent(message));
+            }
+        }
+
+    }
+}
