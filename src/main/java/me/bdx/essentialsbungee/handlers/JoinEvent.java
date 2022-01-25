10e9a12817df1a043fed9276f76ec4f65fda7e52
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-01-24 21:53:08 -0500

    fix: remove imports

--- src/main/java/me/bdx/essentialsbungee/handlers/JoinEvent.java
+++ src/main/java/me/bdx/essentialsbungee/handlers/JoinEvent.java
@@ -5,7 +5,6 @@ import me.bdx.essentialsbungee.managers.WhitelistManager;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.LoginEvent;
-import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import java.util.List;
