e90d4366f96678366c781b5d5602b90bb69b3917
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-05-31 16:46:04 -0400

    Add support for lists of reasons why users cannot join

--- src/main/java/me/bdx/essentialsbungee/config/ConfigController.java
+++ src/main/java/me/bdx/essentialsbungee/config/ConfigController.java
@@ -1,7 +1,10 @@
 package me.bdx.essentialsbungee.config;
 
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.config.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigController {
@@ -11,6 +14,28 @@ public class ConfigController {
     public boolean USE_WHITELIST;
     public List<?> WHITELISTED_USERS;
     public String REFUSED_CONNECTION;
+    public List<String> REFUSED_CONNECTION_LIST;
+
+    /**
+     * Sets what the REFUSED_CONNECTION is to support using lists in connection messages
+     */
+    private void setRefusedConnection(){
+
+        if(this.REFUSED_CONNECTION.equals("")){
+            if(this.REFUSED_CONNECTION_LIST == null){
+                this.REFUSED_CONNECTION = "You are not allowed to join.";
+                return;
+            }
+
+            this.REFUSED_CONNECTION = "";
+            StringBuilder builder = new StringBuilder();
+            ProxyServer.getInstance().getConsole().sendMessage(new TextComponent("Running"));
+            for (String s : REFUSED_CONNECTION_LIST) {
+                builder.append(s).append("\n ");
+            }
+            this.REFUSED_CONNECTION = builder.toString();
+        }
+    }
 
     public ConfigController(){
         Configuration config = ConfigLoader.get();
@@ -19,6 +44,8 @@ public class ConfigController {
         this.USE_WHITELIST = config.getBoolean("use-whitelist");
         this.WHITELISTED_USERS =  config.getList("whitelisted-users");
         this.REFUSED_CONNECTION = config.getString("no-connect-message");
+        this.REFUSED_CONNECTION_LIST = config.getStringList("no-connect-message");
+        this.setRefusedConnection();
     }
 
     public void reload(){
@@ -28,6 +55,8 @@ public class ConfigController {
         this.USE_WHITELIST = config.getBoolean("use-whitelist");
         this.WHITELISTED_USERS =  config.getList("whitelisted-users");
         this.REFUSED_CONNECTION = config.getString("no-connect-message");
+        this.REFUSED_CONNECTION_LIST = config.getStringList("no-connect-message");
+        this.setRefusedConnection();
     }
 
 
