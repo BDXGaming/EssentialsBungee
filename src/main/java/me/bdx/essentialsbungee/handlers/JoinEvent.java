2e8343cdefc76ebf5afc506b6b7b82c697befad3
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-05-31 10:19:22 -0400

    Log all attempted connection uuids

--- src/main/java/me/bdx/essentialsbungee/handlers/JoinEvent.java
+++ src/main/java/me/bdx/essentialsbungee/handlers/JoinEvent.java
@@ -3,7 +3,9 @@ package me.bdx.essentialsbungee.handlers;
 import me.bdx.essentialsbungee.Essentialsbungee;
 import me.bdx.essentialsbungee.managers.WhitelistManager;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -18,6 +20,10 @@ public class JoinEvent implements Listener {
 
         if(Essentialsbungee.essentialsbungee.configcontroller.USE_WHITELIST){
             if(!(AllowedUsers.contains(event.getConnection().getUniqueId()))){
+                ProxyServer.getInstance().getConsole().sendMessage(new TextComponent("[EssentialsBungee]: Player with UUID "+
+                        event.getConnection().getUniqueId() + " Tried to connect!"));
+                ProxyServer.getInstance().getLogger().info("Player with UUID "+ event.getConnection().getUniqueId()
+                        + " Tried to connect!");
                 event.setCancelReason(new TextComponent(ChatColor.translateAlternateColorCodes('&',
                         Essentialsbungee.essentialsbungee.configcontroller.REFUSED_CONNECTION)));
                 event.setCancelled(true);
